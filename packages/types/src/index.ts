dd2b08891e146ed7975c19492245d64ea90f1b89
junsik.oh <junsik@hankookilbo.com>
2025-08-26 14:51:11 +0900

    feat: 첫번째 제안

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,3 +1,41 @@
-export interface Ladder {}
-// 최종 우리가 만들 인스턴스 정의
-// 아래에서 만들다가 조금 더 명확해지면 여기에 작성할 예정.
+// 기본구조
+export interface Ladder {
+  "verticalLines": [
+    { "id": 0 },
+    { "id": 1 },
+    { "id": 2 },
+    { "id": 3 },
+  ],
+  "horizontalLines": [
+    {
+      "from": 0,
+      "to": 1,
+      "yFrom": 0.2,
+      "yTo": 0.3,
+    },
+    {
+      "from": 1,
+      "to": 2,
+      "yFrom": 0.5,
+      "yTo": 0.5,
+    },
+    {
+      "from": 2,
+      "to": 3,
+      "yFrom": 0.7,
+      "yTo": 0.6,
+    }
+  ],
+  "players": [
+    { "name": "이찬웅", "startLine": 0 },
+    { "name": "문정재", "startLine": 1 },
+    { "name": "윤유진", "startLine": 2 },
+    { "name": "남창원", "startLine": 3 },
+  ],
+  "results": [
+    { "line": 0, "value": "연차" },
+    { "line": 1, "value": "오전반차" },
+    { "line": 2, "value": "오후반차" },
+    { "line": 3, "value": "정시퇴근" },
+  ]
+}
