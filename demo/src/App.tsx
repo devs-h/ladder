aa1469b81ab8616832fff55eda05e5c42b0a11ad
uiwwsw <uiwwsw@hankookilbo.com>
2025-08-27 14:47:25 +0900

    feat: 렌더러 관련

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,16 +1,30 @@
+// import "./index.css";
+// import { LadderDom } from "@ladder/dom";
+// import { useLayoutEffect, useRef } from "react";
+// const ladderDom = new LadderDom({ inputs: ["1", "2", "3", "4", "5", "6"] });
+// export function App() {
+//   const wrapRef = useRef<HTMLDivElement>(null);
+//   useLayoutEffect(() => {
+//     wrapRef.current?.append(ladderDom.wrapEl);
+//   }, []);
+//   return (
+//     <div className="app">
+//       <div className="logo-container"></div>
+//       <div ref={wrapRef}></div>
+//     </div>
+//   );
+// }
+
+// export default App;
 import "./index.css";
-import { LadderDom } from "@ladder/dom";
+import { LadderReact } from "@ladder/dom";
 import { useLayoutEffect, useRef } from "react";
-const ladderDom = new LadderDom({ inputs: ["1", "2", "3", "4", "5", "6"] });
 export function App() {
-  const wrapRef = useRef<HTMLDivElement>(null);
-  useLayoutEffect(() => {
-    wrapRef.current?.append(ladderDom.wrapEl);
-  }, []);
   return (
     <div className="app">
       <div className="logo-container"></div>
-      <div ref={wrapRef}></div>
+
+      <LadderReact />
     </div>
   );
 }
