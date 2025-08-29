10087ad8b05f6d633b01dd8062e0e5acda52bf16
uiwwsw <uiwwsw@hankookilbo.com>
2025-08-29 17:57:40 +0900

    test: save

--- packages/dom/src/react.tsx
+++ packages/dom/src/react.tsx
@@ -1,25 +1,61 @@
 import type { ILadder } from "@ladder/common";
-import { forwardRef } from "react";
+import {
+  forwardRef,
+  useImperativeHandle,
+  useRef,
+  type MouseEvent,
+  type MouseEventHandler,
+} from "react";
+import bgm from "./assets/bgm.m4a";
 
 /** 외부로 내보낼 ref 메서드 타입 */
-export interface LadderHandle extends HTMLDivElement {}
+export interface LadderHandle {
+  getPlayers: () => ILadder["players"] | undefined;
+  getResults: () => ILadder["results"] | undefined;
+}
 /** 컴포넌트 props */
 export interface LadderReactProps extends Partial<ILadder> {
   children?: React.ReactNode;
   /** 클래스명 커스터마이징 */
   className?: string;
+  onStart?: MouseEventHandler;
+  onPause?: MouseEventHandler;
 }
 
 export const LadderReact = forwardRef<LadderHandle, LadderReactProps>(
-  ({ children, players, results, className }, ref) => {
+  function LadderReact(
+    { children, players, results, className, onStart, onPause },
+    ref
+  ) {
+    const audioRef = useRef<HTMLAudioElement | null>(null);
+    const handleStart = (e: MouseEvent) => {
+      onStart?.(e);
+      audioRef.current?.play();
+      console.log(players, results);
+    };
+    const handleStop = (e: MouseEvent) => {
+      onPause?.(e);
+      audioRef.current?.pause();
+    };
+    // ✅ 외부로 노출할 메서드만 제공 (DOM 상속 X)
+    useImperativeHandle(
+      ref,
+      (): LadderHandle => ({
+        getPlayers: () => players,
+        getResults: () => results,
+      }),
+      [players, results]
+    );
+
     return (
-      <div className={className} ref={ref}>
+      <div className={className}>
         <div>
           {players?.map((player, index) => (
             <input
               type="text"
               key={`player-${index}-${player.poleId}`}
               defaultValue={player.value}
+              onChange={({ target }) => (player.value = target.value)}
             />
           ))}
         </div>
@@ -30,9 +66,12 @@ export const LadderReact = forwardRef<LadderHandle, LadderReactProps>(
               type="text"
               key={`result-${index}-${result.poleId}`}
               defaultValue={result.value}
+              onChange={({ target }) => (result.value = target.value)}
             />
           ))}
         </div>
+        <button onClick={handleStart}>시작</button>
+        <audio ref={audioRef} src={bgm} />
       </div>
     );
   }
