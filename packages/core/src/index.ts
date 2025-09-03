3415c6e8be8eb0244d6646ae9fd671a8f5320d48
mooncw@hankookilbo.com <mooncw@hankookilbo.com>
2025-09-03 10:27:06 +0900

    core func done

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,5 +1,44 @@
-export class LadderCore {
-  constructor() {
-    return {}; // 사다리게임을 만들 수 있는 데이터 넘겨주기
+import type { ILadder } from "@ladder/common";
+
+export const getNextStep = (
+  ladderData: ILadder,
+  curPole: string,
+  curY: number,
+) => {
+  const { bars } = ladderData;
+  if (bars.length === 0 || !bars) return null;
+
+  const sortedBars = bars.sort((a, b) => {
+    const aY = a.y[0];
+    const bY = b.y[0];
+    if (aY === undefined || bY === undefined) return 0;
+    return aY - bY;
+  });
+
+  const targetBars = sortedBars.filter(
+    (bar) => bar.poleIds[0] === curPole && curY < bar.y[0]!,
+  );
+
+  const nextStep = targetBars.find((bar) => bar.y[0]! > curY);
+
+  if (!nextStep) return { nextPole: curPole, nextY: 1 }; // If there is no next step assume that this is final
+
+  return { nextPole: nextStep.poleIds[1], nextY: nextStep.y[1] };
+};
+
+export const getFinalDestination = (ladderData: ILadder, curPole: string) => {
+  const { bars } = ladderData;
+  if (bars.length === 0 || !bars) return null;
+
+  const nextStep = getNextStep(ladderData, curPole, 0);
+  let nextStepResult = nextStep;
+  while (nextStepResult?.nextY !== 1) {
+    nextStepResult = getNextStep(
+      ladderData,
+      nextStepResult!.nextPole!,
+      nextStepResult!.nextY!,
+    );
   }
-}
+
+  return nextStepResult!.nextPole;
+};
