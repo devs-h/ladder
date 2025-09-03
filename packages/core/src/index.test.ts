5c263b0b27061e234c89b882861af6eb44b26cd2
mooncw@hankookilbo.com <mooncw@hankookilbo.com>
2025-09-03 10:27:06 +0900

    core func done

---
+++ packages/core/src/index.test.ts
@@ -0,0 +1,66 @@
+import { test, expect } from "vitest";
+import { getNextStep, getFinalDestination } from "./index";
+const mockLadder = {
+  poles: [{ id: "0" }, { id: "1" }, { id: "2" }, { id: "3" }],
+  bars: [
+    {
+      poleIds: ["0", "1"],
+      y: [0.257, 0.312],
+    },
+    {
+      poleIds: ["1", "2"],
+      y: [0.259, 0.864],
+    },
+    {
+      poleIds: ["1", "2"],
+      y: [0.269, 0.881],
+    },
+    {
+      poleIds: ["2", "3"],
+      y: [0.888, 0.911],
+    },
+    {
+      poleIds: ["1", "2"],
+      y: [0.264, 0.871],
+    },
+
+    {
+      poleIds: ["2", "3"],
+      y: [0.777, 0.351],
+    },
+  ],
+  players: [
+    { value: "이찬웅", poleId: "0" },
+    { value: "문정재", poleId: "1" },
+    { value: "윤유진", poleId: "2" },
+    { value: "남창원", poleId: "3" },
+  ],
+  results: [
+    { value: "연차", poleId: "0" },
+    { value: "오전반차", poleId: "1" },
+    { value: "오후반차", poleId: "2" },
+    { value: "정시퇴근", poleId: "3" },
+  ],
+};
+
+test("getNextStep test ", () => {
+  expect(getNextStep(mockLadder, "0", 0)).toStrictEqual({
+    nextPole: "1",
+    nextY: 0.312,
+  });
+  expect(getNextStep(mockLadder, "1", 0)).toStrictEqual({
+    nextPole: "2",
+    nextY: 0.864,
+  });
+
+  expect(getNextStep(mockLadder, "1", 0.271)).toStrictEqual({
+    //bigger than 0.271 does not exist so it will return 1
+    nextPole: "1",
+    nextY: 1,
+  });
+});
+
+test("getFinalDestination test ", () => {
+  expect(getFinalDestination(mockLadder, "0")).toStrictEqual("1");
+  expect(getFinalDestination(mockLadder, "1")).toStrictEqual("3");
+});
